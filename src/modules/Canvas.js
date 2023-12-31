aaf472517b7147e726e97cfc7979a85a97d25b7e
Alvin Sung <fear_alvin@hotmail.co.uk>
2023-12-31 22:20:28 +0000

    Adding Button to canvas

--- src/modules/Canvas.js
+++ src/modules/Canvas.js
@@ -22,17 +22,26 @@ resourceGroups = resourceGroups.map(x=> uuidv4());
 
 function onClose(e)
 {
-  
+
   console.log("Close Clicked" )
   resourceGroups = resourceGroups.filter(x => x != e.currentTarget.id)
 
   return () => setValue(value => value + 1); // update state to force render
 }
 
+//Variables to add items onto Canvas
+const [{items}, setItems] = useState({ items: [] });
+const addItem = () => {
+  items.push(<div key={items.length}>This is a test</div>);
+  setItems({ items: [...items] });
+}
+
 
 return (
     <div  className="canvas">
-        {resourceGroups.map((_, idx) => <App key={idx} onClose={onClose} ident={resourceGroups[idx]}/>)}
+      <button className = "rg_button" onClick={addItem}>New Resource Group</button>;
+      {items}
+      {resourceGroups.map((_, idx) => <App key={idx} onClose={onClose} ident={resourceGroups[idx]}/>)}
     </div>
   );
 }
